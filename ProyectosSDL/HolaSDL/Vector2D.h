7cbafe3447ecfe15484dbd09938cf66b4a6b9990
Posna <37537187+Posna@users.noreply.github.com>
2018-10-21 13:35:12 +0200

    Añadido class Texture y Vector

---
+++ ProyectosSDL/HolaSDL/Vector2D.h
@@ -0,0 +1,29 @@
+#ifndef VECTOR2D_H_
+#define VECTOR2D_H_
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vector2D
+{
+private:
+	double x = 0;
+	double y = 0;
+
+public:
+	Vector2D() {}
+	Vector2D(double x, double y) :
+		x(x), y(y) {}
+	int getx() const;
+	int gety() const;
+	void normaliza();
+	Vector2D operator+(const Vector2D& v) const;
+	Vector2D operator*(double d) const;
+	double operator*(const Vector2D& v) const;
+
+
+
+};
+
+#endif
